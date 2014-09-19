801dca6c271a3bc773061a4047195eafb887c06e
Coral <maqujun@yahoo.com>
2014-09-19 10:40:24 +0800

    Clean up the code

--- core/src/main/java/com/scqrs/core/repository/RepositoryHandler.java
+++
@@ -1,9 +0,0 @@
-package com.scqrs.core.repository;
-
-import com.lmax.disruptor.EventHandler;
-import com.scqrs.core.aggregate.Aggregate;
-
-public interface RepositoryHandler extends EventHandler<AggregateEvent> {
-    
-    public Aggregate load(Object aggregateId);
-}
